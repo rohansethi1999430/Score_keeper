5bb1ab3a90c0f112132e4d5535e9fca0a28c9751
Rohan <rohansethi1999430@gmail.com>
2021-06-29 17:00:41 +0530

    update

---
+++ src/Score_keeper.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Score_keeper extends Component{
+    constructor(props){
+        super(props);
+        this.state={score:0}
+    }
+    render(){
+        return(
+            <h1>Hii!</h1>
+        )
+    }
+}
+export default Score_keeper;
