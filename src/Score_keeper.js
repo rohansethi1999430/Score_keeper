fe5b99e42e7f625691bb236d4df259a49a73868b
Rohan <rohansethi1999430@gmail.com>
2021-06-29 17:31:07 +0530

    using setState as a callback function

--- src/Score_keeper.js
+++ src/Score_keeper.js
@@ -5,9 +5,27 @@ class Score_keeper extends Component {
         super(props);
         this.state = { score: 0 }
         this.singleKill = this.singleKill.bind(this);
+        this.tripleKill = this.tripleKill.bind(this);
     }
     singleKill() {
         this.setState({ score: this.state.score + 1 })
+    }
+    // tripleKill() {
+    //     this.setState({ score: this.state.score + 1 })
+    //     this.setState({ score: this.state.score + 1 })
+    //     this.setState({ score: this.state.score + 1 })
+    // }
+
+    tripleKill() {
+        this.setState(st => {
+            return { score: st.score + 1 }
+        })
+        this.setState(st => {
+            return { score: st.score + 1 }
+        })
+        this.setState(st => {
+            return { score: st.score + 1 }
+        })
     }
     render() {
         return (
@@ -15,7 +33,8 @@ class Score_keeper extends Component {
                 <h1>
                     Score is: {this.state.score}
                 </h1>
-                <button>Single Kill!!</button>
+                <button onClick={this.singleKill}>Single Kill!!</button>
+                <button onClick={this.tripleKill}>Triple Kill!!</button>
             </div>
         )
     }
